4e94465b9e7f8a060be3fbda19891ce8fd81bbb1
20b01a12c3 <94444339+nvdivya@users.noreply.github.com>
2022-09-02 22:15:46 +0530

    Add files via upload

---
+++ src/components/CardsData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "Real me",
+        imgdata: "https://i.gadgets360cdn.com/products/large/moto-g52-db-709x800-1649827920.jpg",
+        address: "Krishna Street,Hyd",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1175 + order placed from here recently",
+        price: 35000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "Apple",
+        imgdata: "https://images.unsplash.com/photo-1589492477829-5e65395b66cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW9iaWxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+        address: "Anjani Street ,Vij",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 52000,
+        rating: "3.9",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "Oppo",
+        imgdata: "https://www.91-cdn.com/hub/wp-content/uploads/2021/12/moto-edge-x30-specs-feat-2-1024x538.jpg?tr=q-100",
+        address: "GVRroad,Ongole",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed from here recently",
+        price: 70000,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "One Plus",
+        imgdata: "https://www.thechennaimobiles.com/image/cache/catalog/130972-600x600.jpg",
+        address: "Manji Street,Vij",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 70000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Nokia",
+        imgdata: "https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/plp/phones/new-phones/model-banner/p50-black.png",
+        address: "Nokia store",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1050 + order placed from here recently",
+        price: 15000,
+        rating: "2.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Redmi Note 11T 5G",
+        imgdata: "https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1638175387.9941853.jpg",
+        address: "Wraps store, Chines",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1100 + order placed from here recently",
+        price: 23000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: "Realme 9i 5G",
+        imgdata: "https://cdn1.smartprix.com/rx-irixFVEDu-w1200-h1200/rixFVEDu.jpg",
+        address: "Reveal Store , Hyd",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 33000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "Apple i7",
+        imgdata: "https://www.thechennaimobiles.com/image/cache/catalog/ip13pnk-600x600.jpg",
+        address: "ravi street,vizag",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 100000,
+        rating: "3.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "Never Settle",
+        imgdata: "https://cdn1.smartprix.com/rx-iiTV4RWcM-w1200-h1200/iTV4RWcM.jpg",
+        address: "Slimon street",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2525 + order placed from here recently",
+        price: 50000,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
